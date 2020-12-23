deae3805e26fc4602489ca0883b16e27c8ee5881
Kristi <kristi.martiko@fshnstudent.info>
2020-12-23 17:17:05 +0100

    No Roles

--- springFarmaci/src/main/java/com/example/springFarmaci/dto/UserDTO.java
+++ springFarmaci/src/main/java/com/example/springFarmaci/dto/UserDTO.java
@@ -8,12 +8,12 @@ public class UserDTO {
     public String role;
 
     public UserDTO(String firstName, String lastName, String email,
-                   String password, String role) {
+                   String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.role = role;
+        //this.role = role;
     }
 
     public void setFirstName(String firstName) {
