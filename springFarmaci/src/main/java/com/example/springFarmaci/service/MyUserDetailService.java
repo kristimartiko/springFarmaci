c779b916aea9fa504b44ccfad3ec28b86f3a5f04
Kristi <kristi.martiko@fshnstudent.info>
2020-12-24 19:50:23 +0100

    Final, rolesMap added

--- springFarmaci/src/main/java/com/example/springFarmaci/service/MyUserDetailService.java
+++ springFarmaci/src/main/java/com/example/springFarmaci/service/MyUserDetailService.java
@@ -1,14 +1,21 @@
 package com.example.springFarmaci.service;
 
+import com.example.springFarmaci.models.Roles;
 import com.example.springFarmaci.models.User;
 import com.example.springFarmaci.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
@@ -23,8 +30,26 @@ public class MyUserDetailService implements UserDetailsService {
         if(user == null) {
         }
 
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
 
+    public String getCurrentUserName(String username) {
+        User user = userRepository.findByEmail(username);
+        return user.getFirstName();
+    }
 
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByEmail(auth.getName());
+    }
+
+    public String getCurrentlyLoggedInUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Roles> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
 }
